65f8a4b9f70d1493e7eb3084cb83da293245d70f
Nemond <koloss200@gmail.com>
2022-10-22 08:07:03 +0300

    First commit

--- src/main/java/quru/qa/HelloWorld.java
+++ src/main/java/quru/qa/HelloWorld.java
@@ -3,7 +3,18 @@ package quru.qa;
 public class HelloWorld {
 
     public static void main(String[] args) {
+        String stringThatBeDysplaedInConsole = "Hello World"; //строка
 
+        //можно сперва обьявить переменную, а потом в нее положить
+        // String stringThatBeDysplaedInConsole;
+        //stringThatBeDysplaedInConsole = "Hello World";
+
+        boolean flag = true; //истина
+        // boolean flag0 = false; //ложь
+
+
+
+
+        System.out.println(stringThatBeDysplaedInConsole);
     }
-
 }
